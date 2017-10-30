2f99f3ef0bc4fc093978654282828b370c421008
ern19 <aarontrammell19@gmail.com>
2017-10-30 17:53:18 -0400

    seeded db with fake ass users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030203609) do
+ActiveRecord::Schema.define(version: 20171030213332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorite_movies", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_favorite_movies_on_movie_id"
+    t.index ["user_id"], name: "index_favorite_movies_on_user_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -42,4 +51,6 @@ ActiveRecord::Schema.define(version: 20171030203609) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorite_movies", "movies"
+  add_foreign_key "favorite_movies", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Movie.destroy_all
 
+movies = []
+users = []
+
+5.times do
+    users << User.create(
+        email: FFaker::Internet.email,
+        password: 'blahblah',
+        password_confirmation: 'blahblah'
+    )
+end
+
 Movie.generate("tt0120815")
 Movie.generate("tt1396484")
 Movie.generate("tt0108941")
@@ -14,9 +25,17 @@ Movie.generate("tt2724064")
 Movie.generate("tt0088763")
 Movie.generate("tt0083944")
 Movie.generate("tt0407887")
-Movie.generate("tt0111161")
-Movie.generate("tt0468569")
-Movie.generate("tt0137523")
-Movie.generate("tt0110912")
-Movie.generate("tt1375666")
+movies << Movie.generate("tt0111161")
+movies << Movie.generate("tt0468569")
+movies << Movie.generate("tt0137523")
+movies << Movie.generate("tt0110912")
+movies << Movie.generate("tt1375666")
 
+users.each do |user|
+    movies.each do |movie|
+        FavoriteMovie.create(
+            user_id: user.id,
+            movie_id: movie.id
+        )
+    end
+end
